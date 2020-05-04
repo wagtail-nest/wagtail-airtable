69c4ebf206f43adf06e9d8eff27fdafdf4dfad1f
Kalob Taulien <kalob.taulien@gmail.com>
2020-05-04 10:37:19 -0600

    Auto-register airtable import admin url

--- wagtail_airtable/wagtail_hooks.py
+++ wagtail_airtable/wagtail_hooks.py
@@ -1,6 +1,16 @@
+from django.conf.urls import url
 from django.urls import reverse
 from wagtail.core import hooks
 from wagtail.admin.menu import MenuItem
+
+from wagtail_airtable.views import AirtableImportListing
+
+
+@hooks.register('register_admin_urls')
+def register_airtable_url():
+    return [
+        url(r'^airtable-import/$', AirtableImportListing.as_view(), name='airtable_import_listing'),
+    ]
 
 
 @hooks.register("register_settings_menu_item")
