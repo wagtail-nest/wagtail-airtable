ce28580eb6fdac263f2db9b1e7357e7c0b5db9b3
Kalob Taulien <kalob.taulien@gmail.com>
2020-05-12 12:43:20 -0600

    Hide Import menu option when disabled

--- wagtail_airtable/wagtail_hooks.py
+++ wagtail_airtable/wagtail_hooks.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import url
 from django.urls import reverse
 from wagtail.core import hooks
@@ -15,9 +16,15 @@ def register_airtable_url():
 
 @hooks.register("register_settings_menu_item")
 def register_airtable_setting():
-    return MenuItem(
+
+    def is_shown(request):
+        return settings.WAGTAIL_AIRTABLE_ENABLED
+
+    menu_item = MenuItem(
         "Airtable Import",
         reverse("airtable_import_listing"),
         classnames="icon icon-cog",
         order=1000,
     )
+    menu_item.is_shown = is_shown
+    return menu_item
