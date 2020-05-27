cac919b28918e8bcf73e0e69213c22cb7133b87a
Kalob Taulien <kalob.taulien@gmail.com>
2020-05-27 15:09:44 -0600

    Wording and function argument tweaks

--- wagtail_airtable/templatetags/wagtail_airtable_tags.py
+++ wagtail_airtable/templatetags/wagtail_airtable_tags.py
@@ -5,9 +5,9 @@ from django.conf import settings
 register = template.Library()
 
 @register.simple_tag
-def can_import_model(model_path) -> bool:
+def can_import_model(model_label) -> bool:
     """
-    Check if a model can be imported based on it's model label.
+    Check if a model can be imported based on its model label.
 
     Use:
     {% load wagtail_airtable_tags %}
@@ -16,5 +16,5 @@ def can_import_model(model_path) -> bool:
     Returns True or False.
     """
     airtable_settings = getattr(settings, "AIRTABLE_IMPORT_SETTINGS", {})
-    has_settings = airtable_settings.get(model_path, False)
+    has_settings = airtable_settings.get(model_label, False)
     return bool(has_settings)
