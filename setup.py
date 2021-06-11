8b80294611ebf8567ab8e2cd4e9f996a7cf5ece2
Kalob Taulien <kalob.taulien@gmail.com>
2021-06-11 14:25:03 -0600

    Version bump to 0.2.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = [
 
 setup(
     name='wagtail-airtable',
-    version='0.2',
+    version='0.2.1',
     description="Sync data between Wagtail and Airtable",
     author='Kalob Taulien',
     author_email='kalob.taulien@torchbox.com',
